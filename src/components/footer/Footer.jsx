8282e85c429ef5fca779b30b2014ce2a8ddbf832
sayeemabdullah <sayeem.mdabdullah@gmail.com>
2021-03-19 00:24:30 +0600

    Updated Footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,25 +1,52 @@
 import React from "react";
 import { makeStyles } from "@material-ui/styles";
 import logo from "./Spotify_Logo_NavBar.png";
+import FacebookIcon from "@material-ui/icons/Facebook";
+import InstagramIcon from "@material-ui/icons/Instagram";
+import TwitterIcon from "@material-ui/icons/Twitter";
+import { spacing } from "@material-ui/system";
 
 const styles = makeStyles({
   default: {
     backgroundColor: "black",
     padding: "10rem",
   },
-  firstList: {
+  table: {
     marginLeft: "15rem",
     marginTop: "-5rem",
     color: "white",
+    fontSize: "0.9rem",
+    width: "40%",
+    border: "0.5rem",
+    padding: "0.5rem",
+    borderSpacing: "1.4rem",
+    fontWeight: "bold",
   },
-  secondList: {
-    marginLeft: "25rem",
-    marginTop: "-7.6rem",
-    color: "white",
+  tableHead: {
+    color: "#808385",
+    fontWeight: "bold",
+    fontSize: "0.8rem",
+  },
+  tableBody: {
+    "&:hover": {
+      color: "#2FB150",
+    },
   },
   image: {
     height: "2rem",
     marginTop: "-10rem",
+  },
+  socialNetworks: {
+    color: "white",
+    position: "absolute",
+    right: "20rem",
+    bottom: "-5rem",
+  },
+  socialNetwork: {
+    marginLeft: "2rem",
+    "&:hover": {
+      color: "#2FB150",
+    },
   },
 });
 
@@ -29,39 +56,43 @@ export default function Footer() {
     <>
       <div className={classes.default}>
         <img src={logo} className={classes.image}></img>
-
-        {/* <dl className={classes.firstList}>
-          <dd
-            style={{
-              color: "white",
-              opacity: "0.6",
-              marginBottom: "0.5rem",
-              fontSize: "0.8rem",
-            }}
-          >
-            COMPANY
-          </dd>
-          <dd style={{ marginBottom: "0.7rem" }}>About</dd>
-          <dd style={{ marginBottom: "0.7rem" }}>Jobs</dd>
-          <dd style={{ marginBottom: "0.7rem" }}>For the Record</dd>
-        </dl>
-        <dl className={classes.secondList}>
-          <dd
-            style={{
-              color: "white",
-              opacity: "0.6",
-              marginBottom: "0.5rem",
-              fontSize: "0.8rem",
-            }}
-          >
-            COMMUNITIES
-          </dd>
-          <dd style={{ marginBottom: "0.7rem" }}>For Artists</dd>
-          <dd style={{ marginBottom: "0.7rem" }}>Developers</dd>
-          <dd style={{ marginBottom: "0.7rem" }}>Advertising</dd>
-          <dd style={{ marginBottom: "0.7rem" }}>Investors</dd>
-          <dd style={{ marginBottom: "0.7rem" }}>Vendors</dd>
-        </dl> */}
+        <table className={classes.table}>
+          <tr className={classes.tableHead}>
+            <td>COMPANY</td>
+            <td>COMMUNITIES</td>
+            <td>USEFUL LINKS</td>
+          </tr>
+          <tr>
+            <td className={classes.tableBody}>About</td>
+            <td className={classes.tableBody}>For Artists</td>
+            <td className={classes.tableBody}>Support</td>
+          </tr>
+          <tr>
+            <td className={classes.tableBody}>Jobs</td>
+            <td className={classes.tableBody}>Developers</td>
+            <td className={classes.tableBody}>Web Player</td>
+          </tr>
+          <tr>
+            <td className={classes.tableBody}>For the Record</td>
+            <td className={classes.tableBody}>Advertising</td>
+            <td className={classes.tableBody}>Free Mobile App</td>
+          </tr>
+          <tr className={classes.tableBody}>
+            <td></td>
+            <td>Investors</td>
+            <td></td>
+          </tr>
+          <tr className={classes.tableBody}>
+            <td></td>
+            <td>Vendors</td>
+            <td></td>
+          </tr>
+        </table>
+        <div className={classes.socialNetworks}>
+          <InstagramIcon className={classes.socialNetwork}></InstagramIcon>
+          <TwitterIcon className={classes.socialNetwork}></TwitterIcon>
+          <FacebookIcon className={classes.socialNetwork}></FacebookIcon>
+        </div>
       </div>
     </>
   );
